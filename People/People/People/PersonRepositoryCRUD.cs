cd6d31f1a9b76301c300649d07eac209cd4b202d
Centro de Desarrollo De Software <cds@univo.edu.sv>
2019-09-05 09:08:55 -0600

    PersonRepository

---
+++ People/People/People/PersonRepositoryCRUD.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using People.Models;
+
+namespace People
+{
+    class PersonRepositoryCRUD
+    {
+        SQLiteConnection conn;
+        public string StatusMessage { get; set; }
+
+        public PersonRepositoryCRUD(string dbPath)
+        {
+            // Creamos la conexion 
+            conn = new SQLiteConnection(dbPath);
+            conn.CreateTable<Person>();
+        }
+    }
+}
